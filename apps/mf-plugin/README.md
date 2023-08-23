e8b5a080e8a9e534aeabbcd7c935e9cb76c7c30b
guokeqin <guokeqin163@163.com>
2023-08-23 12:01:15 +0800

    包版本升级

---
+++ apps/mf-plugin/README.md
@@ -0,0 +1,11 @@
+# `mf-plugin`
+
+> TODO: description
+
+## Usage
+
+```
+const mfPlugin = require('mf-plugin');
+
+// TODO: DEMONSTRATE API
+```
--- apps/qiankun-plugin/__tests__/qiankun-plugin.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const qiankunPlugin = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(qiankunPlugin(), 'Hello from qiankunPlugin');
-console.info('qiankunPlugin tests passed');
